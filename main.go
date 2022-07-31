97103a9f853549c466adbfc130fae70a4ef12ed9
idk <hankhill19580@gmail.com>
2022-07-30 23:24:29 -0400

    add default css

--- main.go
+++ main.go
@@ -1,1 +1,52 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/eyedeekay/edgar/tohtml"
+)
+
+var (
+	filename = flag.String("filename", "README.md", "The markdown file to convert to HTML")
+	author   = flag.String("author", authorDefault(), "The author of the HTML file")
+	css      = flag.String("css", "style.css", "The CSS file to use, a default will be generated if one doesn't exist")
+	script   = flag.String("script", "", "The script file to use")
+	title    = flag.String("title", "", "The title of the HTML file")
+)
+
+func authorDefault() string {
+	cmd := exec.Command("git", "config", "--get", "user.email")
+	out, err := cmd.Output()
+	if err != nil {
+		return "unknown"
+	}
+	return strings.Split(string(out), "@")[0]
+}
+
+func main() {
+	flag.Parse()
+	if *title == "" {
+		var err error
+		*title, err = tohtml.ReadFirstMarkdownHeader("README.md")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	output := tohtml.OutputHTMLOpen()
+	output += tohtml.OutputHeaderOpen()
+	output += tohtml.OutputTitleTag(*title)
+	output += tohtml.OutputMetaTag("author", *author)
+	output += tohtml.OutputCSSTag(*css)
+	output += tohtml.OutputScriptTag(*script)
+	output += tohtml.OutputHeaderClose()
+	output += tohtml.OutputBodyOpen()
+	output += tohtml.OutputBodyClose()
+	output += tohtml.OutputHTMLClose()
+	fmt.Println(output)
+}
--- tohtml/tohtml.go
+++ tohtml/tohtml.go
@@ -1,7 +1,10 @@
 package tohtml
 
 import (
+	"fmt"
 	"io/ioutil"
+	"os"
+	"strings"
 
 	"github.com/russross/blackfriday/v2"
 )
@@ -11,11 +14,21 @@ func ReadFirstMarkdownHeader(fileName string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	node, err := blackfriday.Parse(bytes)
+	markdown := blackfriday.New()
+	node := markdown.Parse(bytes)
 	if err != nil {
 		return "", err
 	}
-
+	var title string
+	// walk the nodes until we find an h1, then return the text
+	node.Walk(func(node *blackfriday.Node, entering bool) blackfriday.WalkStatus {
+		if node.Type == blackfriday.Heading && node.Level == 1 {
+			title = strings.Split(node.FirstChild.String(), ": ")[1]
+			return blackfriday.Terminate
+		}
+		return blackfriday.GoToNext
+	})
+	return title, nil
 }
 
 func OutputHTMLOpen() string {
@@ -35,6 +48,12 @@ func OutputMetaTag(name, content string) string {
 }
 
 func OutputCSSTag(cssFile string) string {
+	if _, err := os.Stat(cssFile); err != nil {
+		err := ioutil.WriteFile(cssFile, []byte(DefaultCSS), 0644)
+		if err != nil {
+			fmt.Errorf("Error writing default CSS file: %s", err)
+		}
+	}
 	return "<link rel=\"stylesheet\" type=\"text/css\" href=\"" + cssFile + "\" />"
 }
 
