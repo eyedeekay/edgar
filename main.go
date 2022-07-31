6f1f866d9546637df1b997084b430ba8ca1553e7
idk <hankhill19580@gmail.com>
2022-07-31 16:35:37 -0400

    improve repo name discovery

--- main.go
+++ main.go
@@ -137,7 +137,6 @@ func runGenerator(file, out string) {
 			gitCommitCmd := exec.Command("git", "commit", "-am", "update "+out)
 			if out, err := gitCommitCmd.Output(); err != nil {
 				fmt.Printf("Git Commit Error: %s %s", out, err)
-				os.Exit(1)
 			}
 			if err := enableGithubPage(); err != nil {
 				if strings.Contains(err.Error(), "409") {
@@ -154,36 +153,68 @@ func runGenerator(file, out string) {
 	}
 }
 
+func findGithubRemote() string {
+	// list all the remotes
+	cmdRemotes := exec.Command("git", "remote", "-v")
+	rout, err := cmdRemotes.Output()
+	if err != nil {
+		return ""
+	}
+	var remote string
+	// find the github remote
+	for _, line := range strings.Split(string(rout), "\n") {
+		if strings.Contains(line, "github.com") {
+			// store the remote name
+			remote = strings.Split(line, "\t")[0]
+			fmt.Printf("Found remote %s\n", remote)
+			break
+		}
+	}
+	fmt.Printf("Looked up Github Remote: %s\n", remote)
+	return remote
+}
+
 func findGithubUsername() string {
-	cmd := exec.Command("git", "remote", "get-url", "origin")
+	remote := findGithubRemote()
+	cmd := exec.Command("git", "remote", "get-url", remote)
 	out, err := cmd.Output()
 	if err != nil {
 		return ""
 	}
+	var user string
 	// trim away the scheme, domain
-	user := strings.SplitN(string(out), "github.com", 2)[1]
+	user = strings.SplitN(string(out), "github.com", 2)[1]
+
 	// trim away the leading separators
 	user = strings.TrimLeft(string(user), "/")
 	user = strings.TrimLeft(string(user), ":")
+
 	// split at the /
 	user = strings.Split(string(user), "/")[0]
 	return strings.Replace(user, "\n", "", -1)
 }
 
 func findGithubRepoName() string {
-	cmd := exec.Command("git", "remote", "get-url", "origin")
+	remote := findGithubRemote()
+	fmt.Printf("Remote: '%s'\n", remote)
+	cmd := exec.Command("git", "remote", "get-url", remote)
 	out, err := cmd.Output()
 	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 		return ""
 	}
+
 	// trim away the scheme, domain
 	repo := strings.SplitN(string(out), "github.com", 2)[1]
+
 	// trim away the leading separators
 	repo = strings.TrimLeft(string(repo), "/")
 	repo = strings.TrimLeft(string(repo), ":")
+
 	// split at the /
 	repo = strings.Split(string(repo), "/")[1]
 	repo = strings.TrimRight(string(repo), ".git")
+
 	return strings.Replace(repo, "\n", "", -1)
 }
 
