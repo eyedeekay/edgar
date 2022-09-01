2d04e2add0fc5e5e3de21b8d1b8f20b7c7d69802
idk <hankhill19580@gmail.com>
2022-09-01 19:49:16 -0400

    update index.html

--- tohtml/tohtml.go
+++ tohtml/tohtml.go
@@ -257,20 +257,25 @@ func OutputSourceRepos() string {
 
 func OutputDonationURLs(donate, donatemessage string) string {
 	split := strings.Split(donate, ",")
-	ret := "<div id=\"donate\">"
+	ret := "<div id=\"donatediv\">"
 	ret += "  <div id=\"donatemessage\">"
-	ret += donatemessage
+	ret += "  <a href=\"#donate\">" + donatemessage + "</a>"
 	ret += "  </div>"
+	ret += "  <div id=\"donate\">"
+	ret += "  <div id=\"hidedonate\">"
 	for _, addr := range split {
-		ret += "  <div class=\"wallet-addr\">"
-		ret += "  <a href=\"" + addr + "\">"
+		ret += "    <div class=\"wallet-addr\">"
+		ret += "      <a href=\"" + addr + "\">"
 		ret += strings.Split(addr, ":")[0]
-		ret += "  </a>"
-		ret += "  <div id=\"" + strings.Split(addr, ":")[0] + "\">"
+		ret += "      </a>"
+		ret += "      <span id=\"" + strings.Split(addr, ":")[0] + "\">"
 		ret += addr
-		ret += "  </div>"
-		ret += "  </div>"
+		ret += "      </span>"
+		ret += "    </div>"
 	}
+	ret += "  <a href=\"#hidedonate\">" + "Close donation panel" + "</a>"
+	ret += "  </div>"
+	ret += "  </div>"
 	ret += "</div>"
 	return ret
 }
