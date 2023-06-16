434275ac8fcd3cdc6484e635fce587ee95107763
idk <idk@yoga7.idk.i2p>
2023-06-16 18:40:51 -0400

    page generation update for: 2023-06-16 18:40:51.782173779 -0400 EDT m=+0.267783975

--- tohtml/tohtml.go
+++ tohtml/tohtml.go
@@ -241,8 +241,14 @@ func NavigationBar(files []string, out string) string {
 		log.Println("checking file for navbar inclusion ", file)
 		if count > 1 {
 			log.Println("count, file", count, file)
-			spl := strings.Split(file, "/")[count-1:]
-			file = strings.Join(spl, "/")
+			if count-1 > len(strings.Split(file, "/")) {
+				spl := strings.Split(file, "/")[count-1:]
+				file = strings.Join(spl, "/")
+			} else {
+				spl := strings.Split(file, "/")[:]
+				file = strings.Join(spl, "/")
+			}
+
 		}
 		if strings.HasSuffix(file, ".md") {
 			title := strings.Split(strings.ReplaceAll(file, "README", "index"), ".")[0]
