bd30aa925619d07b19b4de72bedf02f661254d66
eyedeekay <idk@mulder>
2024-02-07 11:20:34 -0500

    Wait just a little longer between downloads because I know for a fact I haven't made 5000 API calls since 10:20 this morning @github.

--- github/download.go
+++ github/download.go
@@ -149,7 +149,7 @@ func DownloadReleaseAssets(release Release) ([]string, error) {
 			continue
 		}
 		names = append(names, name)
-		time.Sleep(15 * time.Second)
+		time.Sleep(30 * time.Second)
 	}
 	log.Println("Downloaded", names, "assets")
 	return names, nil
